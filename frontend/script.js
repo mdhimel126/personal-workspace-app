8d224ad360c69d579321e4ba4c126ec57f9f3f17
mdhimel126 <mdhimel6467@gmail.com>
2026-07-22 22:27:09 +0600

    Add 3 icon in contact menu

--- frontend/script.js
+++ frontend/script.js
@@ -5,3 +5,10 @@ btn1.addEventListener("click",()=>{
     window.location.href="tracking.html";
 });
 
+const menu=document.querySelector(".contact-menu");
+const msgBtn=document.querySelector("#message-icon");
+
+msgBtn.addEventListener("click",()=>{
+    menu.classList.toggle("active");
+});
+
